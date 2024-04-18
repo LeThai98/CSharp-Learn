78b721c3f6df077e27f3b10905aa0c176fd8f18a
lethai <hongthaiit1998@gmail.com>
2024-04-18 15:11:06 +0700

    Dependency Injection example

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿using static System.Console;
+﻿using Microsoft.Extensions.DependencyInjection;
+using static System.Console;
 using static System.Math;
-
-
 using X = MyNameSpace;
 
 namespace MyApp
@@ -10,15 +9,84 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-           Samppham.Product product = new Samppham.Product("Laptop", 1000);
-           product.description = "Fake Product";
-              WriteLine(product.GetInfo());
-                WriteLine(product.description);
+            //Breaf breaf = new Breaf();
+            //Breakfast breakfast = new Breakfast(breaf);
+            //breakfast.Eat();
 
-            Samppham.Product.ManuFactory manu = new Samppham.Product.ManuFactory();
-            manu.Name = "Samppham";
-            manu.Address = "Vietnam";
-            WriteLine(manu.Name);
+
+            // initial DI Container
+            IServiceCollection services = new ServiceCollection();
+
+            // register services
+            services.AddSingleton<IBreaf, Breaf1>();
+            //services.AddSingleton<IBreaf, Breaf2>();
+            services.AddSingleton<Breakfast>();
+
+            // build Service Provider
+            var provider = services.BuildServiceProvider();
+            
+            // get instance of service from Service Provider
+            //var breaf = provider.GetService<IBreaf>();
+
+            Breakfast breakfast = provider.GetService<Breakfast>();
+            breakfast.Eat();
+
+
+
+
         }
     }
+
+    internal class Breakfast
+    {
+        private IBreaf breaf;
+
+        public Breakfast(IBreaf _breaf)
+        {
+            breaf = _breaf;
+        }
+
+        public void Eat()
+        {
+            breaf.NuongBreak();
+            Console.WriteLine("Yummy");
+        }
+    }
+
+    public interface IBreaf
+    {
+        void NuongBreak();
+    }
+
+    internal class Breaf1: IBreaf
+    {
+        private string bot;
+
+        //public Breaf(string _bot)
+        //{
+        //    this.bot = _bot;
+        //}
+
+        public void NuongBreak()
+        {
+            Console.WriteLine("Nuong Xeo Xeo");
+        }
+    }
+
+    internal class Breaf2 : IBreaf
+    {
+        private string bot;
+
+        //public Breaf(string _bot)
+        //{
+        //    this.bot = _bot;
+        //}
+
+        public void NuongBreak()
+        {
+            Console.WriteLine("Nuong Thom Phuc");
+        }
+    }
+
+
 }
