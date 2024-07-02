9e9737b04265f98939038f83ca78a248c6e7a2f1
LeThai98 <hongthaifamily@gmail.com>
2024-07-02 13:35:21 +0700

    git ignore

--- obj/Debug/net8.0/CSharp-Learn.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharp-Learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp-Learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81618dfe13add19ab8137e047f51dc30175d60c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp-Learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp-Learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
